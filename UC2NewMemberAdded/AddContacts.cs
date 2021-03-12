2286143716f6c841456b934982f3190feb6c9c58
Shashi <shashihghebad3@gmail.com>
2021-03-12 08:28:43 -0800

    [SHASHI]Add:UC2-NewMemberAdded

---
+++ UC2NewMemberAdded/AddContacts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AddressBook
+{
+    internal class AddContacts
+    {
+        internal void Assign(string first_Name, string last_Name, string address, string city, string state, int zip, int phone_number, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Show()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UC2NewMemberAdded/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AddressBook
+{
+    class Program
+    {
+        public static string first_Name;
+        public static string last_Name;
+        public static string address;
+        public static string city;
+        public static string state;
+        public static int zip;
+        public static int phone_number;
+        public static string email;
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" WELCOME TO ADDRESS BOOK SYSTEM PROGRAM \n");
+
+            Console.WriteLine(" How many people's Information is needed to be Added(Expected Integer): \n");
+            int NUM_OF_PEOPLE = Convert.ToInt32(Console.ReadLine());
+
+            AddContacts obj = new AddContacts();
+
+            for (int i = 1; i <= NUM_OF_PEOPLE; i++)
+            {
+                Console.WriteLine("\n Write First Name of the person: ");
+                first_Name = Console.ReadLine();
+                Console.WriteLine("\n Write Last Name of the person: ");
+                last_Name = Console.ReadLine();
+                Console.WriteLine("\n Write Address of the person: ");
+                address = Console.ReadLine();
+                Console.WriteLine("\n Write City of the person: ");
+                city = Console.ReadLine();
+                Console.WriteLine("\n Write State of the person: ");
+                state = Console.ReadLine();
+                Console.WriteLine("\n Write Zip of the person: ");
+                zip = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\n Write Phone_number of the person: ");
+                phone_number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\n Write Email of the person: ");
+                email = Console.ReadLine();
+
+                obj.Assign(first_Name, last_Name, address, city, state, zip, phone_number, email);
+            }
+            obj.Show();
+            Console.ReadKey();
+        }
+    }
+}
