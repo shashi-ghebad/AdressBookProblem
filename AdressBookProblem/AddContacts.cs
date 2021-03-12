0cf66fd7c2b7ead1f852278c44720928faa74262
Shashi <shashihghebad3@gmail.com>
2021-03-12 01:34:50 -0800

    [SHASHI]Add:UC1-CreateContacts

---
+++ AdressBookProblem/AddContacts.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    class AddContacts
+    {
+        private List<TakeContacts> list = new List<TakeContacts>();
+
+        public void Assign(string first_Name, string last_Name, string address, string city, string state, int zip, int phone_number, string email)
+        {
+            TakeContacts class_object = new TakeContacts();
+            class_object.FirstName = first_Name;
+            class_object.LastName = last_Name;
+            class_object.Address = address;
+            class_object.City = city;
+            class_object.State = state;
+            class_object.Zip = zip;
+            class_object.Phone_number = phone_number;
+            class_object.Email = email;
+
+            list.Add(class_object);
+        }
+
+        public void Show()
+        {
+            foreach (TakeContacts item in list)
+            {
+                Console.WriteLine("\n The first Name of person is: " + item.FirstName);
+                Console.WriteLine("The last name of the person is: " + item.LastName);
+                Console.WriteLine("The Address of the person is: " + item.Address);
+                Console.WriteLine("The city of the person is: " + item.City);
+                Console.WriteLine("The State of the person is: " + item.State);
+                Console.WriteLine("The zip od the person is: " + item.Zip);
+                Console.WriteLine("The phone number of the person is: " + item.Phone_number);
+                Console.WriteLine("The email of the person is: " + item.Email);
+            }
+        }
+    }
+}
---
+++ AdressBookProblem/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" WELCOME TO ADDRESS BOOK SYSTEM PROGRAM \n");
+
+            Console.WriteLine("Write First Name of the person: ");
+            string first_Name = Console.ReadLine();
+            Console.WriteLine(" \n Write Last Name of the person: ");
+            string last_Name = Console.ReadLine();
+            Console.WriteLine("\n Write Address of the person: ");
+            string address = Console.ReadLine();
+            Console.WriteLine("\n Write City of the person: ");
+            string city = Console.ReadLine();
+            Console.WriteLine("\n Write State of the person: ");
+            string state = Console.ReadLine();
+            Console.WriteLine("\n Write Zip of the person: ");
+            int zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n Write Phone_number of the person: ");
+            int phone_number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n Write Email of the person: ");
+            string email = Console.ReadLine();
+
+            AddContacts obj = new AddContacts();
+            obj.Assign(first_Name, last_Name, address, city, state, zip, phone_number, email);
+            obj.Show();
+
+            Console.ReadKey();
+        }
+    }
+}
