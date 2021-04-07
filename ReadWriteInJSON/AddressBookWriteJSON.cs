58daca1117d5d595d3a93e0cc038b7b6a59822dc
Shashi <shashihghebad3@gmail.com>
2021-04-07 13:45:32 +0530

    [SHASHI]Add:UC15-ReadWriteInJSON

---
+++ ReadWriteInJSON/AddressBookWriteJSON.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using CsvHelper;
+using Newtonsoft.Json;
+
+
+namespace ReadWriteInJSON
+{
+    class AddressBookWriteJSON
+    {
+        public static void ImplementCSVToJSON()
+        {
+            string importFilePath = @"C:\Users\Sashi\Desktop\AdressBoook\AdressBookProblem\ReadWriteInJSON\Utility\AddressBook.csv";
+            string exportFilePath = @"C:\Users\Sashi\Desktop\AdressBoook\AdressBookProblem\ReadWriteInJSON\Utility\AddressBookJSON.csv";
+            using (var reader = new StreamReader(importFilePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<TakeContacts>().ToList();
+
+                Newtonsoft.Json.JsonSerializer serializer = new Newtonsoft.Json.JsonSerializer();
+                using (StreamWriter sw = new StreamWriter(exportFilePath))
+                using (JsonWriter writer = new JsonTextWriter(sw))
+                {
+                    serializer.Serialize(writer, records);
+                }
+            }
+
+        }
+
+        public static void ImplementCSVToJSONAndRead()
+        {
+            string FilePath = @"C:\Users\Sashi\Desktop\AdressBoook\AdressBookProblem\ReadWriteInJSON\Utility\AddressBookJSON.csv";
+
+            using (StreamReader sr = File.OpenText(FilePath))
+            {
+                String s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+
+        }
+    }
+}
--- ReadWriteInJSON/TakeContacts.cs
+++ ReadWriteInJSON/TakeContacts.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace ReadWriteInCSVFile
+namespace ReadWriteInJSON
 {
     public class TakeContacts
     {
